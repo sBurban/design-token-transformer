063fd90dceade91580867e7fa1624e5d73ce1d0a
sBurban <sBurban@users.noreply.github.com>
2022-01-11 15:19:02 +0000

    Tokens updated

--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -4,15 +4,36 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Tue, 11 Jan 2022 15:19:01 GMT
 
 
 #import "StyleDictionarySize.h"
 
 
-float const TypographyBodyH3FontWeight = 700;
-float const TypographyBodyH4StrikeThroughFontWeight = 500;
-float const TypographyBodyItalicFontWeight = 400;
-float const TypographyBodyExtraBoldCondensedItalicFontWeight = 800;
-float const TypographyBodyMediumExtendedItalicFontWeight = 500;
-float const TypographyBodySuperFontWeight = 900;
+float const TypographyH1BoldFontWeight = 700;
+float const TypographyH1LightFontWeight = 300;
+float const TypographyH1RegularFontWeight = 400;
+float const TypographyH2BoldFontWeight = 700;
+float const TypographyH2LightFontWeight = 300;
+float const TypographyH2RegularFontWeight = 400;
+float const TypographyH3BoldFontWeight = 700;
+float const TypographyH3LightFontWeight = 300;
+float const TypographyH3RegularFontWeight = 400;
+float const TypographyH4BoldFontWeight = 700;
+float const TypographyH4RegularFontWeight = 400;
+float const TypographyH4LightFontWeight = 300;
+float const TypographyH5BoldFontWeight = 700;
+float const TypographyH5LightFontWeight = 300;
+float const TypographyH5RegularFontWeight = 400;
+float const TypographyBodyBoldFontWeight = 700;
+float const TypographyBodyLightFontWeight = 300;
+float const TypographyBodyRegularFontWeight = 400;
+float const TypographyBodySmBoldFontWeight = 700;
+float const TypographyBodySmLightFontWeight = 300;
+float const TypographyBodySmRegularFontWeight = 400;
+float const TypographyBodyXsBoldFontWeight = 700;
+float const TypographyBodyXsLightFontWeight = 300;
+float const TypographyBodyXsRegularFontWeight = 400;
+float const TypographyLinkBoldFontWeight = 700;
+float const TypographyLinkLightFontWeight = 300;
+float const TypographyLinkRegularFontWeight = 400;
